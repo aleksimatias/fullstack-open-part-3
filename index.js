f963f4af64c0df1c23e12a9d9b45304e7f4421e4
Aleksi Männistö <aatumannisto@gmail.com>
2023-01-12 23:59:18 +0200

    Step 3.2. Phonebook backend step2

--- index.js
+++ index.js
@@ -30,6 +30,19 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  res.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>
+  `);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  res.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
